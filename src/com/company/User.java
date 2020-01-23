cf178644bf2d096636eabfb9ff43215f9269bc96
Anna Söllscher <annamaria.sollscher@gmail.com>
2020-01-23 23:44:47 +0100

    Added some comments in book/library

--- src/com/company/User.java
+++ src/com/company/User.java
@@ -2,6 +2,7 @@ package com.company;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+
 //implementerar Serializable för att kunna läsa om till fil
 public class User implements Serializable {
     //Privat attribut med name, lista av böcker som den användare har lånat (borrowedBooks)
@@ -16,23 +17,23 @@ public class User implements Serializable {
     public String getName() {
         return name;
     }
-    /**metod som visar lånade böcker
+
+    /**metod som visar lånade böcker, borrowBook
     *Tar emot boken som ska lånas
-    *Anropar setBorroed (true) för att visa att boken är lånad
+    *Anropar book.setBorroed (true) för att visa att boken är lånad
     *Lägger till den lånade boken i listan borrowedBooks(borrowedBooks.add)
      */
     public void borrowBook(Book book) {
         book.setBorrowed(true);
         borrowedBooks.add(book);
     }
-    //Skickar tillbaka en lista (returnerar den) med alla lånade böcker, (en kopia av listan)
+    //Metod, med en arraylista av book som Returnerar en lista  med alla lånade böcker, (en kopia av listan)
     public ArrayList<Book> getBorrowedBooks() {
         return new ArrayList<>(borrowedBooks);
     }
-    /** metod som tar emot en bok som ska lämnas tillbaka
-     * Returnerar boken som användaren lämnar tillbaka
-    *Tar emot ett index för boken som ska lämnast tillbaka
-    *Tar bort boken från listan av lånade böcker,(borrowedBooks.remove)
+    /** metod som tar emot en bok som ska lämnas tillbaka, via index (returnBook)
+     * Returnerar boken som användaren lämnar tillbaka, return borrowedBooks
+    *Tar bort boken från listan av lånade böcker via index,(borrowedBooks.remove(index))
      */
     public Book returnBook (int index) {
         return borrowedBooks.remove(index);
